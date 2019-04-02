c7a0f4414b1e7c3f8c132ae4c84ca761c879acb2
Sebastian <sebanicosp@gmail.com>
2019-04-02 18:02:19 -0300

    Add: card notas

--- lib/ui/calcular_notas.dart
+++ lib/ui/calcular_notas.dart
@@ -20,18 +20,63 @@ class _CalcularNotasState extends State<CalcularNotas> {
           decoration: BoxDecoration(color: Colors.black87),
           child: Stack(
             children: <Widget>[
-              Container(
-                alignment: Alignment(0, -1),
-                child: NotaFinal(),
-              ),
               ListView(
                 children: <Widget>[
                   Container(
                     margin: EdgeInsets.only(top: 220),
-                    
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 100.0,
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          child: Card(
+                            color: Colors.black,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  top: 10.0,
+                                  left: 25.0,
+                                  child: Text(
+                                    "Nota 1",
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                      color: Colors.white
+                                    ),
+                                  ),                                  
+                                ),
+                                Positioned(
+                                  top: 40.0,
+                                  right: 35.0,
+                                  child: Text(
+                                    "%",
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                      color: Colors.white
+                                    ),
+                                  ),                                  
+                                ),
+                                Positioned(
+                                  left: 25.0,
+                                  top: 40.0,
+                                  child: ,)
+                                
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   )
                 ],
-              )
+              ),
+              Container(
+                alignment: Alignment(0, -1),
+                child: NotaFinal(),
+              ),
             ],
           )),
     );
